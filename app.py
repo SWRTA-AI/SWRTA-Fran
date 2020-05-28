e905326c3b0140b0db1f5dc3380ba23144bd4ec1
jessinra <jessinra@gmail.com>
2020-05-28 21:10:18 +0700

    refactor

--- app.py
+++ app.py
@@ -1,41 +1,13 @@
 
-import pickle
-import numpy as np
 from flask import Flask, request, jsonify, abort
 from flask_cors import CORS
 
 from src.client import tf_serving
 from src.model.tokenizer import Tokenizer
-from src.model.picks import PickPredictorModel
-from src.model.bans_01 import BanPredictorModel01
-from src.model.bans_02 import BanPredictorModel02
-from src.model.unitRelation import UnitRelationPredictor
-from src.util.bestiary import Bestiary
-from src.util.output_formatter import OutputFormatter
+from loader import model_pick, model_ban_01, model_ban_02, bestiary, output_formatter, model_friend, model_counterpick
 
 app = Flask(__name__)
 CORS(app)
-
-tokenizer = pickle.load(open("pickle/tokenizer.pkl", "rb"))
-model_pick = PickPredictorModel(tokenizer)
-model_ban_01 = BanPredictorModel01(tokenizer)
-model_ban_02 = BanPredictorModel02(tokenizer)
-bestiary = Bestiary("resource/bestiary.json")
-output_formatter = OutputFormatter(bestiary)
-
-pick_together_matrix = np.load("pickle/pick_matrix.npy")
-counter_pick_matrix = np.load("pickle/counterpick_matrix.npy")
-log_pick_count = np.load("pickle/log_pick_count.npy")
-model_friend = UnitRelationPredictor(
-    tokenizer=tokenizer,
-    pick_matrix=pick_together_matrix,
-    log_pick_count=log_pick_count
-)
-model_counterpick = UnitRelationPredictor(
-    tokenizer=tokenizer,
-    pick_matrix=counter_pick_matrix,
-    log_pick_count=log_pick_count
-)
 
 
 @app.route('/', methods=['GET', 'POST'])
